0a3e1f0358109d4a955d6d529a15566b29e0fe4c
Commander (fbsd.fritz.box) <root@mx.buetow.org>
2014-06-15 14:06:07 +0200

    uptimed works with fbsd

--- uptimed/manifests/files.pp
+++ uptimed/manifests/files.pp
@@ -1,9 +1,13 @@
 class uptimed::files (
   $ensure,
 ) {
-  File { owner => root, group => root, mode => '0644' }
+  File { 
+    owner => root, 
+    group => $::inter::rootgroup,
+    mode => '0644',
+  }
 
-  file { '/etc/uptimed.conf':
+  file { "${::inter::defaultprefix}/etc/uptimed.conf":
     ensure  => $ensure,
     content => template('uptimed/uptimed.conf.erb'),
   }
