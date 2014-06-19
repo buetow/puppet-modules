b4505972a797f80c838839038a3c6ce20e49968c
Paul C. Buetow (mars.fritz.box) <paul@buetow.org>
2014-06-20 00:08:02 +0200

    add support for multiple motd templates

--- motd/manifests/init.pp
+++ motd/manifests/init.pp
@@ -1,9 +1,26 @@
-class motd {
+class motd (
+  $ensure = present,
+  $template = '',
+){
+  if $template == '' {
+    case $::operatingsystem {
+      FreeBSD: { $template_file = 'motd.rantanplanbsd.erb' }
+      NetBSD: { $template_file = 'motd.rantanplanbsd.erb' }
+      OpenBSD: { $template_file = 'motd.rantanplanbsd.erb' }
+      DragonFly: { $template_file = 'motd.rantanplanbsd.erb' }
+      Debian: { $template_file = 'motd.rantanplanian.erb' }
+      Ubuntu: { $template_file = 'motd.rantanplanian.erb' }
+      default: { $template_file = 'motd.erb' }
+    }
+  } else {
+    $template_file = $template
+  }
+
   file { '/etc/motd':
-    ensure  => present,
-    content => template('motd/motd.erb'),
+    ensure  => $ensure,
+    content => template("motd/${template_file}"),
     mode    => '0444',
     owner   => root,
-    group   => $root_group,
+    group   => $::root_group,
   }
 }
