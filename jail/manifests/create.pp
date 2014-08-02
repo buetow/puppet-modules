800846f19ab67b70214485032dcab822d43b2071
admin (sedna.buetow.org) <puppet@mx.buetow.org>
2014-08-02 09:23:09 +0200

    Quick commit

--- jail/manifests/create.pp
+++ jail/manifests/create.pp
@@ -59,6 +59,8 @@ define jail::create (
         mode    => '0755',
         owner   => root,
         group   => $root_group,
+
+        require => Zfs::Create["${zfs_tank}${mountpoint}"],
       }
     }
   }
