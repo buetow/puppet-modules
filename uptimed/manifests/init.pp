28546b4173afc549c324e538643cdee13e132617
Chuck Norris (uranus.fritz.box) <paul@buetow.org>
2014-09-13 08:21:56 +0200

    Today is the first day of the rest of your life.

--- uptimed/manifests/init.pp
+++ uptimed/manifests/init.pp
@@ -2,20 +2,52 @@ class uptimed (
   $ensure = present,
   $records_file = '/var/spool/uptimed/records',
 ) {
+  case $ensure {
+    'running': {
+      $ensure_package = present
+      $ensure_file = present
+      $ensure_directory = directory
+      $ensure_service = running
+      $ensure_enabled = enabled
+      $service_enable = true
+    }
+    'stopped': {
+      $ensure_package = present
+      $ensure_file = present
+      $ensure_directory = directory
+      $ensure_service = stopped
+      $service_enable = false
+    }
+    'present': {
+      $ensure_package = present
+      $ensure_file = present
+      $ensure_directory = directory
+      $ensure_service = stopped
+      $service_enable = false
+    }
+    'absent': {
+      $ensure_package = absent
+      $ensure_file = absent
+      $ensure_directory = absent
+      $ensure_service = stopped
+      $service_enable = false
+    }
+  }
+
 
   class { 'uptimed::packages':
-    ensure => $ensure,
+    ensure => $ensure_package,
   }
 
   class { 'uptimed::files':
-    ensure => $ensure,
+    ensure => $ensure_file,
 
     require => Class['uptimed::packages'],
   }
 
   service { 'uptimed':
-    ensure => running,
-    enable => true,
+    ensure => $ensure_service,
+    enable => $ensure_enable,
 
     require => Class['uptimed::files'],
   }
--- uptimed/manifests/packages.pp
+++ uptimed/manifests/packages.pp
@@ -1,13 +1,7 @@
 class uptimed::packages (
   $ensure,
 ){
-
-  case $ensure {
-    present: { $package_ensure = installed }
-    default: { $package_ensure = purged }
-  }
-
   package { 'uptimed':
-    ensure => $package_ensure,
+    ensure => $ensure,
   }
 }
