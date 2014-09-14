e7f75ab88c26e27c16f0f9401df6a6ae485b9840
Chuck Norris (uranus.fritz.box) <paul@buetow.org>
2014-09-14 13:20:08 +0200

    Don't look back, the lemmings are gaining on you.

--- ports/manifests/init.pp
+++ ports/manifests/init.pp
@@ -4,19 +4,26 @@ class ports (
   $symlink = '/usr/ports',
   $use_zfs = true,
   $zfs_tank = 'zroot',
+
 ) {
   case $ensure {
     present: {
+      $ensure_file = file
       $ensure_directory = directory
       $ensure_link = link
     }
     absent: {
+      $ensure_file = absent
       $ensure_directory = absent
       $ensure_link = absent
     }
     default: {
       fail("No such ensure: ${ensure}")
     }
+  }
+
+  Exec {
+    path => '/bin:/sbin:/usr/bin:/usr/sbin/'
   }
 
   file { $mountpoint:
@@ -36,6 +43,29 @@ class ports (
     noop       => !$use_zfs,
 
     require    => File[$mountpoint],
+  }
+
+  $portsbootstrap = "${zfs_tank}${mountpoint}/.portsbootstrap"
+  $bootstrapdone = "${portsbootstrap}/bootstrap.done"
+
+  file { $portsbootstrap:
+    ensure  => $ensure_directory,
+    require => Zfs::Create["${zfs_tank}${mountpoint}"],
+  }
+
+  exec { 'portsnap fetch':
+    require => File[$portsbootstrap],
+    unless  => "test -f ${bootstrapdone}"
+  }
+
+  exec { 'portsnap extract':
+    require => Exec['portsnap fetch'],
+    unless  => "test -f ${bootstrapdone}"
+  }
+
+  file { $bootstrapdone:
+    ensure  => $ensure_file,
+    require => Exec['portsnap extract'],
   }
 }
 
