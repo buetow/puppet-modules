44fc3c1109bbf1c9c2358c6cc480df741602983c
Chuck Norris (sun.buetow.org) <puppet@mx.buetow.org>
2014-10-04 13:40:54 +0200

    Quick commit

---
+++ mailman_freebsd/manifests/init.pp
@@ -0,0 +1,61 @@
+# This module has been tested on FreeBSD 10 only
+class mailman_freebsd (
+  $service = 'mailman',
+  $package = 'mailman', 
+  $ensure = running,
+) {
+  File {
+    owner => root,
+    group => $root_group,
+    mode  => '0644',
+  }
+
+  case $ensure {
+    'running': {
+      $ensure_package = present
+      $ensure_file = present
+      $ensure_directory = directory
+      $ensure_service = running
+      $ensure_enabled = enabled
+      $service_enable = true
+    }
+    'stopped': {
+      $ensure_package = present
+      $ensure_file = present
+      $ensure_directory = directory
+      $ensure_service = stopped
+      $service_enable = false
+    }
+    'present': {
+      $ensure_package = present
+      $ensure_file = present
+      $ensure_directory = directory
+      $ensure_service = stopped
+      $service_enable = false
+    }
+    'absent': {
+      $ensure_package = absent
+      $ensure_file = absent
+      $ensure_directory = absent
+      $ensure_service = stopped
+      $service_enable = false
+    }
+  }
+
+  package { $package:
+    ensure => $ensure_package
+  }
+
+  service { $service:
+    enable  => $service_enable,
+    ensure  => $ensure_service,
+  
+    require => [
+      Package[$package],
+    ],
+  }
+
+  freebsd::rc_enable { $service:
+    ensure => $ensure_file,
+  }
+}
