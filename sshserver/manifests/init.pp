d7f158278d31ec9e23530e311bcde9624ba92973
Paul Buetow (uranus.fritz.box) <paul@buetow.org>
2014-11-20 21:22:56 +0000

    foo

--- sshserver/manifests/init.pp
+++ sshserver/manifests/init.pp
@@ -4,34 +4,39 @@ class sshserver (
   $allow_users = 'admin',
 ) {
 
-  if $operatingsystem == 'FreeBSD' {
-    $initname = 'sshd'
+  if $operatingsystem == 'Fedora' {
+    notify { 'Fedota is not yet supported': 
+    }
   } else {
-    $initname = 'ssh'
-  }
+    if $operatingsystem == 'FreeBSD' {
+      $initname = 'sshd'
+    } else {
+      $initname = 'ssh'
+    }
 
-  class { 'sshserver::packages':
-    ensure => $ensure
-  }
+    class { 'sshserver::packages':
+      ensure => $ensure
+    }
 
-  file { "${name}_sshd_config":
-    ensure  => $ensure,
-    content => template('sshserver/sshd_config.erb'),
-    path    => '/etc/ssh/sshd_config',
-    owner   => root,
-    group   => $root_group,
-    mode    => '0644',
+    file { "${name}_sshd_config":
+      ensure  => $ensure,
+      content => template('sshserver/sshd_config.erb'),
+      path    => '/etc/ssh/sshd_config',
+      owner   => root,
+      group   => $root_group,
+      mode    => '0644',
 
-    require => Class['sshserver::packages'],
-  }
+      require => Class['sshserver::packages'],
+    }
 
-  service { "${name}_ssh":
-    ensure    => running,
-    name      => $initname,
-    enable    => true,
-    hasstatus => true,
+    service { "${name}_ssh":
+      ensure    => running,
+      name      => $initname,
+      enable    => true,
+      hasstatus => true,
 
-    require   => [File["${name}_sshd_config"],Class['sshserver::packages']],
-    subscribe => File["${name}_sshd_config"],
+      require   => [File["${name}_sshd_config"],Class['sshserver::packages']],
+      subscribe => File["${name}_sshd_config"],
+    }
   }
 }
