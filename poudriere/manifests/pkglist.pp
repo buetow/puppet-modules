9e8f3eac80c58107126aad13614e17ef11db0b53
Paul Buetow (sun) <puppet@mx.buetow.org>
2015-04-19 23:56:50 +0200

    add myconf

--- poudriere/manifests/pkglist.pp
+++ poudriere/manifests/pkglist.pp
@@ -1,10 +1,17 @@
 define poudriere::pkglist (
   $ensure = present,
+  $mybulk_helper_manage = true,
   $pkglist = [],
   $dest_dir = '/usr/local/etc/poudriere.d/myconf',
 ) {
   file { "${dest_dir}/${name}-pkglist":
     ensure  => $ensure,
     content => template("poudriere/pkglist.erb"),
+  }
+  if $mybulk_helper_manage {
+    file { "${dest_dir}/${name}-bulk.sh":
+      ensure  => $ensure,
+      source  => "puppet:///modules/poudriere/myconf/${name}-bulk.sh",
+    }
   }
 }
