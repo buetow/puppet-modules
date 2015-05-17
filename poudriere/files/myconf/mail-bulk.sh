aac6bb2b8bb5985087103ca1c61e62759ff30ca2
Paul Buetow (europa) <paul@buetow.org>
2015-05-17 14:43:33 +0100

    Things worth having are worth cheating for.

--- poudriere/files/myconf/mail-bulk.sh
+++ poudriere/files/myconf/mail-bulk.sh
@@ -3,5 +3,6 @@
 set -x
 
 poudriere ports -u -p local
+#poudriere options -j 10amd64 -p local -z mail -f mail-pkglist
 poudriere bulk -c -j 10amd64 -p local -z mail -f mail-pkglist
 #poudriere bulk -j 10amd64 -p local -z mail -f mail-pkglist
