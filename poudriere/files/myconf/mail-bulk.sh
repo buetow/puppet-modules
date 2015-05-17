649663293129902e57f2c7bb0fadfe3ddd0d2ef0
Paul Buetow (sun) <puppet@mx.buetow.org>
2015-05-17 16:47:10 +0200

    Quick commit

--- poudriere/files/myconf/mail-bulk.sh
+++ poudriere/files/myconf/mail-bulk.sh
@@ -5,5 +5,5 @@ set -x
 # Need postfix with SASL and mailman with postfix support.
 
 poudriere ports -u -p local
-poudriere options -j 10amd64 -p local -z mail -f mail-pkglist
+#poudriere options -j 10amd64 -p local -z mail -f mail-pkglist
 poudriere bulk -j 10amd64 -p local -z mail -f mail-pkglist
