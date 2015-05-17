8df356cfc422dd8e64641730389128a243308b74
Paul Buetow (europa) <paul@buetow.org>
2015-05-17 14:46:42 +0100

    I have accepted Provolone into my life!

--- poudriere/files/myconf/mail-bulk.sh
+++ poudriere/files/myconf/mail-bulk.sh
@@ -2,7 +2,8 @@
 
 set -x
 
-poudriere ports -u -p local
+#poudriere ports -u -p local
+#poudriere bulk -c -j 10amd64 -p local -z mail -f mail-pkglist
+
 #poudriere options -j 10amd64 -p local -z mail -f mail-pkglist
-poudriere bulk -c -j 10amd64 -p local -z mail -f mail-pkglist
-#poudriere bulk -j 10amd64 -p local -z mail -f mail-pkglist
+poudriere bulk -j 10amd64 -p local -z mail -f mail-pkglist
