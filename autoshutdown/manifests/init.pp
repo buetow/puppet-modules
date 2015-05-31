9b39ccb49aad4e848f479d11099827e52b32bae0
Paul Buetow (europa) <paul@buetow.org>
2015-05-31 18:12:37 +0100

    I just know I'm a better manager when I have Joe DiMaggio in center field. 		-- Casey Stengel

--- autoshutdown/manifests/init.pp
+++ autoshutdown/manifests/init.pp
@@ -2,6 +2,7 @@ class autoshutdown (
     $ensure = present,
     $up_hours = 6,
     $halt_command = '/sbin/halt -p',
+    $test_path = '/bin/test',
 ) {
   case $ensure {
     present: {
@@ -9,6 +10,7 @@ class autoshutdown (
         exec { 'shutdown':
           command => $halt_command,
           user    => root,
+          unless  => "${test_path} -f /var/run/autoshutdown.disable",
         }
       }
     }
