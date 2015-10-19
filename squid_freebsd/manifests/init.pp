c5e44f6398a316e1016741d07c7e7f875af874cc
Paul Buetow (uranus) <paul@buetow.org>
2015-10-19 07:47:38 +0100

    add squid.conf.erb

--- squid_freebsd/manifests/init.pp
+++ squid_freebsd/manifests/init.pp
@@ -1,12 +1,20 @@
 class squid_freebsd (
   $ensure = present,
+  $http_port = '127.0.0.1:3128',
 ) {
 
   package { 'squid':
     ensure => $ensure,
   }
 
+  file { '/usr/local/etc/squid/squid.conf':
+    ensure  => $ensure,
+    content => template('squid_freebsd/squid.conf.erb'),
+  }
+
   freebsd::rc_enable { 'squid':
-    ensure => $ensure,
+    ensure    => $ensure,
+    subscribe => File['/usr/local/etc/squid/squid.conf'],
+    require   => File['/usr/local/etc/squid/squid.conf'],
   }
 }
