2f5cb65bb0bdece051dbaa156a553f957b4ea26c
Paul Buetow (icinga) <puppet@mx.buetow.org>
2015-11-14 10:24:28 +0000

    initial icinga2 modulefor freebsd

---
+++ icinga2_freebsd/manifests/init.pp
@@ -0,0 +1,63 @@
+# This module has been tested on FreeBSD 10 only
+class icinga2_freebsd (
+  $package = 'icinga2',
+  $ensure = running,
+) {
+
+  File {
+    owner => root,
+    group => $root_group,
+    mode  => '0644',
+  }
+
+  $service = $package
+
+  case $ensure {
+    'running': {
+      $ensure_package = present
+      $ensure_file = present
+      $ensure_directory = directory
+      $ensure_link = link
+      $ensure_service = running
+      $ensure_enabled = enabled
+      $service_enable = true
+    }
+    'stopped': {
+      $ensure_package = present
+      $ensure_file = present
+      $ensure_directory = directory
+      $ensure_link = link
+      $ensure_service = stopped
+      $service_enable = false
+    }
+    'present': {
+      $ensure_package = present
+      $ensure_file = present
+      $ensure_directory = directory
+      $ensure_link = link
+      $ensure_service = stopped
+      $service_enable = false
+    }
+    'absent': {
+      $ensure_package = absent
+      $ensure_file = absent
+      $ensure_directory = absent
+      $ensure_link = absent
+      $ensure_service = stopped
+      $service_enable = false
+    }
+  }
+
+  package { $package:
+    ensure => $ensure_package
+  }
+
+  service { $service:
+    enable  => $service_enable,
+    ensure  => $ensure_service,
+    require => [
+      Package[$package],
+    ],
+  }
+}
+
