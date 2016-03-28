d47351021bed2f47716de91a21f312854c0963fb
Paul Buetow (uranus) <paul@buetow.org>
2016-03-28 22:17:18 +0100

    initial bind for freebsd

---
+++ bind_freebsd/manifests/init.pp
@@ -0,0 +1,76 @@
+# This module has been tested on FreeBSD 10 only
+class bind_freebsd (
+  $package = 'bind910',
+  $ensure = running,
+  $conf_d_source = '',
+) {
+
+  File {
+    owner => root,
+    group => $root_group,
+    mode  => '0644',
+  }
+
+  $service = $package
+
+  case $ensure {
+    'running': {
+      $ensure_package = present
+      $ensure_file = present
+      $ensure_directory = directory
+      $ensure_link = link
+      $ensure_service = running
+      $ensure_enabled = enabled
+      $service_enable = true
+    }
+    'stopped': {
+      $ensure_package = present
+      $ensure_file = present
+      $ensure_directory = directory
+      $ensure_link = link
+      $ensure_service = stopped
+      $service_enable = false
+    }
+    'present': {
+      $ensure_package = present
+      $ensure_file = present
+      $ensure_directory = directory
+      $ensure_link = link
+      $ensure_service = stopped
+      $service_enable = false
+    }
+    'absent': {
+      $ensure_package = absent
+      $ensure_file = absent
+      $ensure_directory = absent
+      $ensure_link = absent
+      $ensure_service = stopped
+      $service_enable = false
+    }
+  }
+
+  package { $package:
+    ensure => $ensure_package
+  }
+
+  if $conf_d_source != '' {
+    file { '/usr/local/etc/bind/conf.d':
+      ensure    => $ensure_directory,
+      purge     => true,
+      recurse   => true,
+      force     => true,
+      source    => $conf_d_source,
+      require   => Package[$package],
+      notify    => Service[$service],
+    }
+  }
+
+  service { $service:
+    enable  => $service_enable,
+    ensure  => $ensure_service,
+    require => [
+      Package[$package],
+    ],
+  }
+}
+
