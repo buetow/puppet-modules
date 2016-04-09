e02c9b453328a4e6be4236c28507a08388cc97d6
Paul Buetow (sun) <default@mx.buetow.org>
2016-04-09 09:44:19 +0200

    ensure periodic.conf exists

--- freebsd/manifests/periodic_config.pp
+++ freebsd/manifests/periodic_config.pp
@@ -2,9 +2,23 @@ define freebsd::periodic_config (
   $value,
   $ensure = present,
 ){
+  $periodic_file = '/etc/periodic.conf'
+
+  unless (defined(File[$periodic_file])) {
+    file { $periodic_file:
+      ensure  => $ensure,
+      content => '',
+      replace => false,
+      mode    => '0644',
+      owner   => root,
+      group   => wheel,
+      before  => File_line[$name],
+    }
+  }
+
   file_line { $name:
     ensure  => $ensure,
     line    => "${name}=${value}",
-    path    => '/etc/periodic.conf',
+    path    => $periodic_file,
   }
 }
