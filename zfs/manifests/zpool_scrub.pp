95972349b1e6683c55422fadd46b3102115527ab
Paul Buetow (uranus) <paul@buetow.org>
2016-04-09 08:29:23 +0100

    add zpool_scrub

---
+++ zfs/manifests/zpool_scrub.pp
@@ -0,0 +1,18 @@
+define zfs::zpool_scrub (
+  $ensure = present,
+  $threshold = 7
+){
+  Exec {
+    path => '/sbin:/usr/sbin:/usr/local/sbin:/bin:/usr/bin:/usr/local/bin'
+  }
+
+  freebsd::periodic_config { 'daily_scrub_zfs_enable':
+    ensure => $ensure,
+    value  => 'YES',
+  }
+
+  freebsd::periodic_config { 'daily_scrub_zfs_default_threshold':
+    ensure => $ensure,
+    value  => $threshold
+  }
+}
