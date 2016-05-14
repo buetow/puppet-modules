2a2335f6c6f19e2cec4ebcbec42adbc74b5ddaa0
Paul Buetow (alphacentauri) <default@mx.buetow.org>
2016-05-14 08:44:07 +0100

    fortune not found Quick commit

--- uptimed/manifests/packages.pp
+++ uptimed/manifests/packages.pp
@@ -5,9 +5,7 @@ class uptimed::packages (
   # No CentOS packages available, compile manually
   # and install to /usr/local/sbin/uptimed
   if $operatingsystem != 'CentOS' {
-    include pkgng
     package { 'uptimed':
-      provider => pkgng,
       ensure   => $ensure,
     }
   }
